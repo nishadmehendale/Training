154ab8c830fef5a148352a5d626e8aa363e4e2c8
nishadmehendale <nishadmehendale@gmail.com>
2018-07-19 22:38:53 +0530

    Example of Class Method and Object concept

---
+++ employee.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class salary
+{
+	private final static int id;
+	private string name;
+	private double monthlyBasic;
+	private double hra = 0.5 * monthlyBasic;
+	private double medicalAllowance = 1250.0;
+	private double conveyenceAllowance = 800.0;
+	private double PF=0.0;
+	if (0.1 * monthlyBasic < 6500.0)
+	{
+		
+	}
+	double getAnnualBasic()
+	{
+		return 12*monthlyBasic;
+	}
+	double getMonthlyGrossSalary()
+	{
+		return 
+	}
+	void getAnnualGrossSalary()
+	{
+		
+	}
+	void getMonthlyDeductions()
+	{
+		
+	}
+	void getMonthlyTakeHome()
+	{
+		
+	}
+	void getAnnualTakeHome()
+	{
+		
+	}
+	void setPFRate()
+	{
+		
+	}
+}
+class employee{
+	public static void main(String args [])
+	{
+		Scanner sc= new Scanner(System.in);
+		
+	}
+}
