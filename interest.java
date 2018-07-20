4d3257510d6689c11269eaa7a0e0715f03756313
nishadmehendale <nishadmehendale@gmail.com>
2018-07-20 21:06:09 +0530

    Updated Files

--- interest.java
+++ interest.java
@@ -3,16 +3,16 @@ class interest
 {
 	public static void main(String args [])
 	{
-		Scanner sc= new Scanner(system.in);
+		Scanner sc= new Scanner(System.in);
 		System.out.println("Enter principle amount");
 		int p = sc.nextInt();
 		System.out.println("Enter rate of interest");
 		float r = sc.nextFloat();
 		System.out.println("Enter number of years");
 		int n = sc.nextInt();
-		float s,c,a;
-		s = (float)(p*n*r)/100;
-		a = p*(Math.pow((1+rate/100),n));
+		double s,c,a;
+		s = (double)(p*n*r)/100;
+		a = (double)p*Math.pow((1f+r/100f),n);
 		c = a - p;
 		System.out.println("Simple Interest");
 		System.out.println(s);
--- marks.java
+++ marks.java
@@ -3,18 +3,18 @@ class marks
 {
 	public static void main(String args[])
 	{
-		Scanner sc= new Scanner(system.in);
+		Scanner sc= new Scanner(System.in);
 		System.out.println("Enter marks of subject 1");
-		m1 = sc.nextInt();
+		int m1 = sc.nextInt();
 		System.out.println("Enter marks of subject 2");
-		m2 = sc.nextInt();
+		int m2 = sc.nextInt();
 		System.out.println("Enter marks of subject 3");
-		m3 = sc.nextInt();
+		int m3 = sc.nextInt();
 		if(m1>60 && m2>60 && m3>60)
 		{
 			System.out.println("Passed");
 		}
-		else if((m1>60 && m2>60) || (m1>60 && m3>60) ||(m2>60 && m3>60)
+		else if((m1>60 && m2>60) || (m1>60 && m3>60) ||(m2>60 && m3>60))
 		{
 			System.out.println("Promoted");
 		}
