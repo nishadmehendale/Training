effeaa617320654dde84c5bf49eb11e3d685732d
nishadmehendale <nishadmehendale@gmail.com>
2018-07-22 10:57:41 +0530

    Synced new files

---
+++ Training examples/src/EmployeeTest.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class EmployeeTest {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Employee Name");
+		String name = scanner.nextLine();
+		System.out.println("Welcome "+ name);
+	}
+
+}
